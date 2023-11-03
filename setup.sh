986592fb41098dd818c97d193ef553cc95ef7d26
ianache <ianache@comsatel.com.pe>
2023-11-03 06:12:48 +0000

    fix: fleet-server y setup de es

--- setup.sh
+++ setup.sh
@@ -19,22 +19,11 @@ if [ ! -f config/certs/ca.zip ]; then
 fi;
 if [ ! -f config/certs/certs.zip ]; then
    echo "Creating certs";
-#   echo -ne \
-#     "instances:\n"\
-#     "  - name: es01\n"\
-#     "    dns:\n"\
-#     "      - es01\n"\
-#     "      - localhost\n"\
-#     "    ip:\n"\
-#     "      - 127.0.0.1\n"\
-#     "  - name: kibana\n"\
-#     "    dns:\n"\
-#     "      - kibana\n"\
-#     "      - localhost\n"\
-#     "    ip:\n"\
-#     "      - 127.0.0.1\n"\
-#     > config/certs/instances.yml;
-   bin/elasticsearch-certutil cert --silent --pem -out config/certs/certs.zip --in config/certs/instances.yml --ca-cert config/certs/ca/ca.crt --ca-key config/certs/ca/ca.key;
+   bin/elasticsearch-certutil cert --silent --pem \
+	   -out config/certs/certs.zip \
+	   --in config/certs/instances.yml \
+	   --ca-cert config/certs/ca/ca.crt \
+	   --ca-key config/certs/ca/ca.key;
    unzip config/certs/certs.zip -d config/certs;
 fi;
 
@@ -44,8 +33,17 @@ find . -type d -exec chmod 750 \{\} \;;
 find . -type f -exec chmod 640 \{\} \;;
 echo "Waiting for Elasticsearch availability";
 
-until curl -s --cacert config/certs/ca/ca.crt https://es01:9200 | grep -q "missing authentication credentials"; do sleep 30; done;
+until curl -s --cacert config/certs/ca/ca.crt https://es01:9200 | \
+	grep -q "missing authentication credentials"; \
+        do sleep 30; \
+done;
 echo "Setting kibana_system password";
-until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://es01:9200/_security/user/kibana_system/_password -d "{\"password\":\"${KIBANA_PASSWORD}\"}" | grep -q "^{}"; do sleep 10; done;
+until curl -s -X POST --cacert config/certs/ca/ca.crt \
+	-u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" \
+	https://es01:9200/_security/user/kibana_system/_password \
+	-d "{\"password\":\"${KIBANA_PASSWORD}\"}" | \
+	grep -q "^{}"; \
+        do sleep 10; \
+done;
 echo "All done!";
 
